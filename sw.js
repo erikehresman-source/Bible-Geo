c3e3fb9502491b7ce85ae36de98cb11e148a40cc
erikehresman-source <226130187+erikehresman-source@users.noreply.github.com>
2025-08-15 20:57:37 -0700

    version 5.2

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'blg-cache-v5.2';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './questions.json',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './images/map_base.svg'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
